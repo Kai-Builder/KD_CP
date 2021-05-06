652c8de30e1ccf994b5a35090e3dfa6cd5965c35
kaigonzalez <gkai70264@gmail.com>
2021-05-06 15:27:27 -0400

    a little user handler

---
+++ userdata/user_handle.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Kai Gonzalez on 5/6/21.
+//
+
+#include "user_handle.h"
+
+
+
+void KD_ServerInit(char** argv)
+{
+    KD_PrintScreen("you have new mail on authenticator " + KD_UserAuthentication);
+}
---
+++ userdata/user_handle.h
@@ -0,0 +1,14 @@
+//
+// Created by Kai Gonzalez on 5/6/21.
+//
+#include <iostream>
+#ifndef KD_RP_USER_HANDLE_H
+#define KD_RP_USER_HANDLE_H
+//add the definiton of printScreen
+// this was made for users who would like to PR, or make their own features.
+extern void KD_PrintScreen(const std::string&);
+
+//. . .
+extern std::string KD_UserAuthentication;
+
+#endif //KD_RP_USER_HANDLE_H
