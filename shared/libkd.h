3ca74422b4121e96cc1b1d7e929763f67f916e2a
kaigonzalez <gkai70264@gmail.com>
2021-05-06 15:26:29 -0400

    add a dynamic lib

---
+++ shared/libkd.h
@@ -0,0 +1,21 @@
+//
+// Created by Kai Gonzalez on 5/6/21.
+//
+
+#ifndef KD_RP_LIBKD_H
+#define KD_RP_LIBKD_H
+#include <string>
+
+enum KD_LowerLevelKeyword
+{
+    BEGIN,
+    QUIT,
+    LEAVE,
+    JSON_DATA
+};
+
+extern void KD_PrintScreen(const std::string&)
+
+//anything else. ...
+
+#endif //KD_RP_LIBKD_H
